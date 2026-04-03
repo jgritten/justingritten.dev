d6ebf7346e04fdfafd57ff8c1122b4c73ec9fcda
Jusitn Gritten <justin.gritten@gmail.com>
2026-04-03 16:56:36 -0700

    Client image replaced

--- client/src/utils/saasClientAssets.ts
+++ client/src/utils/saasClientAssets.ts
@@ -1,2 +1,3 @@
 /** Public asset: banner when no client logo URL is set (guest or unsigned tenant). */
-export const SAAS_GUEST_CLIENT_PLACEHOLDER_LOGO = '/no_client_selected.jpg'
+// export const SAAS_GUEST_CLIENT_PLACEHOLDER_LOGO = '/no_client_selected.jpg'
+export const SAAS_GUEST_CLIENT_PLACEHOLDER_LOGO = '/client.svg'
